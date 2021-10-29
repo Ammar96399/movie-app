e78e13896232cb2d37e80a455b928e5605966712
Ammar <ammar.kazem@etudiant.univ-rennes1.fr>
2021-10-29 22:15:32 +0200

    Finished v0 of front-end app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component, Fragment } from "react";
-import {BrowserRouter as Router, Switch, Route, Link, useParams} from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Route, Link, useParams, useRouteMatch} from 'react-router-dom'
 import Movies from "./components/Movies";
 import Admin from "./components/Admin";
 import Home from "./components/Home";
+import Categories from "./components/Categories";
+import OneMovie from './components/OneMovie'
 
 export default function App() {
   return (
@@ -26,6 +28,9 @@ export default function App() {
                   <Link to="/movies">Movies</Link>
                 </li>
                 <li className="list-group-item">
+                  <Link to="/by-category">Categories</Link>
+                </li>
+                <li className="list-group-item">
                   <Link to="/admin">Manage Catalogue</Link>
                 </li>
               </ul>
@@ -33,12 +38,23 @@ export default function App() {
           </div>
           <div className="col-md-10">
             <Switch>
-              <Route path="/movies/:id">
-                <Movie />
-              </Route>
+              <Route path="/movies/:id" component={OneMovie}/>
               <Route path="/movies">
                 <Movies/>
               </Route>
+              <Route exact path="/by-category">
+                <CategoryPage/>
+              </Route>
+              <Route 
+              exact 
+              path="/by-category/drama"
+              render={(props) => <Categories {...props} title= {`Drama`}/>}
+              />
+              <Route 
+              exact 
+              path="/by-category/comedy"
+              render={(props) => <Categories {...props} title= {`Comedy`}/>}
+              />
               <Route path="/admin">
                 <Admin/>
               </Route>
@@ -53,8 +69,18 @@ export default function App() {
   );
 }
 
-function Movie() {
-  let { id } = useParams();
+function CategoryPage() {
 
-  return <h2> Movie id {id}</h2>
+  let { path, url } = useRouteMatch();
+
+  return(
+    <div>
+      <h2>Categories</h2>
+
+      <ul>
+        <li><Link to={`${path}/drama`}>Drama</Link> </li>
+        <li><Link to={`${path}/comedy`}>Comedy</Link> </li>
+      </ul>
+    </div>
+  );
 }
