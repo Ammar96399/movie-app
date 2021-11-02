d9f0b59516fce23449082f464ce5c0e8cbb55cfe
Ammar <ammar.kazem@etudiant.univ-rennes1.fr>
2021-11-02 10:36:19 +0100

    Added connection with database

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"github.com/julienschmidt/httprouter"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application)	routes() *httprouter.Router {
+func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 
 	router.HandlerFunc(http.MethodGet, "/status", app.statusHandler)
 
+	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie)
+
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
 	return router
 }
